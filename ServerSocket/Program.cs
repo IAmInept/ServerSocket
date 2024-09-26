9bf01e4ecf06e7bad05fe43d18991b50c7e230e0
Gian-Luca Wallace <54045132+IAmInept@users.noreply.github.com>
2024-09-26 17:42:53 +0100

    Add project files.

---
+++ ServerSocket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Text.Encodings;
+
+    namespace ServerSocket
+{
+    class Program
+    {
+        public static void Main()
+        {
+            TCPSocket Client = new TCPSocket();
+            Client.TCPPort = 33333;
+            Client.IPV4Address = "127.0.0.1";
+
+            TcpListener webServer = new TcpListener(Client.IPV4AddrBytes, Client.TCPPort);
+            TcpClient s = new TcpClient(Client.IPV4Address, Client.TCPPort);
+
+            webServer.Start();
+            s.Connect(Client.IPV4Address, Client.TCPPort);
+            
+
+            //Socket ServerSock = new Socket()
+            //NetworkStream TCPStream = new NetworkStream();
+
+        }
+    }
+}
---
+++ ServerSocket/TCPSocket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Text.Encodings;
+
+
+namespace ServerSocket
+{
+    public class TCPSocket()
+    {
+        public int TCPPort { get; set; }
+        public string IPV4Address { get; set; }
+
+        public IPAddress IPV4AddrBytes { get
+            {
+                return IPAddress.Parse(IPV4Address);
+            }
+        }
+
+    }
+}
